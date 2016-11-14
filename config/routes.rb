8f513bde7121312cbff54c55236d6154634178aa
Cameron Stanley <cameronstanley23@gmail.com>
2016-11-14 07:57:32 -0600

    Implement login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static#landing'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
